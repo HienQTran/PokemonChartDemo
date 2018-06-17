1ec1aa7774126a11e4b2c01b63957df514e46bca
Hien Quang Tran <hien.tran@wizeline.com>
2018-06-17 11:18:21 -0500

    prepare for starter project

--- ChartDemoPlayground.playground/Pages/ChartViewController.xcplaygroundpage/Contents.swift
+++ ChartDemoPlayground.playground/Pages/ChartViewController.xcplaygroundpage/Contents.swift
@@ -1,34 +1,55 @@
 //: [Previous](@previous)
 
-import UIKit
-import ChartDemoFramework
-import PlaygroundSupport
 
-let pokemons: [Pokemon] = [
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    Pokemon.pikachu,
 //    Pokemon.alolanRaichu,
-    Pokemon.raichu,
-
+//    Pokemon.raichu,
+//
 //    Pokemon.bulbasaur,
 //    Pokemon.ivysaur,
 //    Pokemon.megaVenusaur,
 //    Pokemon.venusaur,
-
+//
 //    Pokemon.charmander,
-    Pokemon.charmeleon,
+//    Pokemon.charmeleon,
 //    Pokemon.megaCharizardY,
 //    Pokemon.megaCharizardX,
 //    Pokemon.charizard,
-
-    Pokemon.squirtle
+//
+//    Pokemon.squirtle
 //    Pokemon.wartortle,
 //    Pokemon.megaBlastoise,
 //    Pokemon.blastoise
-]
-
-let viewController = ChartViewController()
-viewController.pokemons = pokemons
-viewController.view.frame.size = CGSize(width: 375, height: 812)
-
-PlaygroundPage.current.liveView = viewController.view
